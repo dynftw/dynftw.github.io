dc3d3c31b90a38b51d36d9f8404b9c5a5d1a6bc4
dynftw <iltgh626@hotmail.com>
2019-08-02 21:19:07 +1000

    Redirect downloads.php to downloads.html

---
+++ spectrogram/downloads.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>dynftw - Logitech Spectrogram | Downloads</title>
+	<link rel="shortcut icon" type="image/x-icon" href="../images/icon.png" />
+	<link rel="stylesheet" href="../css/main.css">
+	<link href='https://fonts.googleapis.com/css?family=Oswald|Open+Sans' rel='stylesheet' type='text/css'>
+	<meta http-equiv="Refresh" content="0; url="downloads.html"/>
+	<script type="text/javascript">
+            window.location.href = "http://dynftw.tk/spectrogram/downloads.html"
+     </script>
+</head>
+<body>
+	<div class="headerOuter">
+		<div class="headerInner">
+			<div class="headerLogo">
+				dynftw	
+			</div>
+			<div class="headerNav">
+				<a href="../index.html">home</a>
+				<a href="index.html">spectrogram</a>
+			</div>
+		</div>
+		<div class="headerBottom">
+		</div>
+	</div>
+		<div class="content">
+			If you are not redirected automatically, follow this <a href='downloads.html'>link</a>.
+		</div>
+	<div class="footerTop"></div>
+	<footer>
+		© 2019 dynftw
+	</footer>
+</body>
+</html>
